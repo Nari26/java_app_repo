dd84748b8763ab78757ea9bb108c0b15ee418dfe
Nari26 <43762809+Nari26@users.noreply.github.com>
2021-05-14 02:44:52 -0400

    Update docker.sh

--- jenkins/docker.sh
+++ jenkins/docker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Install docker 
-sudo yum install docker -y 
-sudo systemctl start docker 
-sudo systemctl enable docker 
-sudo systemctl status docker 
+# Install docker
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
 
 mkdir -p $HOME/playground/jcasc
-cp -R . $HOME/playground/jcasc/
+cp * $HOME/playground/jcasc/
 cd $HOME/playground/jcasc
-
+docker stop jenkins
+docker rm jenkins
 docker build -t jenkins:jcasc .
-docker run -d --name jenkins -p 8085:8080 jenkins:jcasc
+docker run -d --name jenkins -p 8085:8080 --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=$1 jenkins:jcasc
